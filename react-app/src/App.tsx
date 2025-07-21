47a98c90be43487d4117521ff240e9377cc79d55
yM3ky <yoysimeky@gmail.com>
2025-07-21 14:59:02 +0700

    hjnhnhb

---
+++ react-app/src/App.tsx
@@ -0,0 +1,28 @@
+import Listgroup from "./components/ListGroup";
+import { MouseEvent } from "react";
+
+function App() {
+  let items = ["Jakarta", "New York", "London", "tokyo"];
+
+  const handleClick = (event: MouseEvent) => console.log(event);
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={handleClick}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
---
+++ react-app/src/components/ListGroup.tsx
@@ -0,0 +1,5 @@
+function Listgroup() {
+    return <h1>List Group</h1>;
+}
+
+export default Listgroup;
